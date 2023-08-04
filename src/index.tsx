1fb48ecdad786b06de3b43bdd185cd69988a203a
Kacper Faber <kacperf1234@gmail.com>
2023-08-04 14:59:10 +0200

    index.tsx added TODO
    Because in API docs there's no theme controller explained.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import {createRoot} from "react-dom/client";
 import {RouterProvider} from "react-router-dom";
 import {Router} from "./router";
 
+// TODO: In API there's no themes, or I don't see it in docs.
+
 function getRootElement(): HTMLElement {
     const root = document.getElementById("app");
     if (!root) throw Error("Root element #app not found.");
