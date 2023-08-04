c24db774bb7e55343fd034f173931721e83f3ab5
Kacper Faber <kacperf1234@gmail.com>
2023-08-04 11:42:23 +0200

    httpUtils.ts - added deleteAsync, putAsync
    putAsync takes 'body: any' parameter.
    
    To all [getAsync, deleteAsync, putAsync] added catch block.

--- src/api/httpUtils.ts
+++ src/api/httpUtils.ts
@@ -7,10 +7,29 @@ export class HttpUtils{
     }
 
     public static getAsync<T>(url: string, token: string | undefined = undefined): Promise<T> {
-        return new Promise<T>((resolve) => {
+        return new Promise<T>((resolve, reject) => {
             fetch(url, {headers: token ? this.headers(token) : new Headers(), method: "GET"})
                 .then(resp => resp.json() as T)
-                .then(resp => resolve(resp));
+                .then(resp => resolve(resp))
+                .catch(() => reject());
+        });
+    }
+
+    public static deleteAsync<T>(url: string, token: string | undefined = undefined): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            fetch(url, {headers: token ? this.headers(token) : new Headers(), method: "DELETE"})
+                .then(resp => resp.json() as T)
+                .then(resp => resolve(resp))
+                .catch(() => reject());
+        });
+    }
+
+    public static putAsync<T>(url: string, body: any, token: string | undefined = undefined): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            fetch(url, {body: body, headers: token ? this.headers(token) : new Headers(), method: "PUT"})
+                .then(resp => resp.json() as T)
+                .then(resp => resolve(resp))
+                .catch(() => reject());
         });
     }
 }
