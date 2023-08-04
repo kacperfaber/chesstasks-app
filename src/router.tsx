e79ce1d5d158c451268ffe66979b123e7764a9ea
Kacper Faber <kacperf1234@gmail.com>
2023-08-04 09:54:34 +0200

    Renamed Home.tsx to home.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import {createBrowserRouter} from "react-router-dom";
-import {Home} from "./ui/routes/home/Home";
+import {Home} from "./ui/routes/home/home";
 
 export const Router = createBrowserRouter([
     {
