6924132096c219d25fb3a58f4b5670b14b91b621
Kacper Faber <kacperf1234@gmail.com>
2023-08-04 11:25:51 +0200

    Modified tsconfig and webpack.config.dev.js
    webpack.config.dev.js:
    - set process.env.CONFIG to {apiUrl} in dev webpack config.
    
    tsconfig.json:
    - rootDir is deleted.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,12 +2,17 @@ const {merge} = require("webpack-merge");
 const base = require("./webpack.config");
 const webpack = require("webpack");
 
+const config = {
+    apiUrl: 'http://localhost:8080'
+}
+
 module.exports = merge(base, {
     mode: 'development',
     devtool: "inline-source-map",
     plugins: [
         new webpack.DefinePlugin({
-            "process.env.PROFILE": JSON.stringify("dev")
+            "process.env.PROFILE": JSON.stringify("dev"),
+            "process.env.CONFIG": JSON.stringify(config)
         })
     ]
 });
