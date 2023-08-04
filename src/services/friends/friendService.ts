62465bd3f88193e311c6b9cc744fbc9018057188
Kacper Faber <kacperf1234@gmail.com>
2023-08-04 13:44:48 +0200

    FriendService.
    Added 'getSentRequests' and 'getReceivedRequests'.

--- src/services/friends/friendService.ts
+++ src/services/friends/friendService.ts
@@ -1,6 +1,7 @@
 import {Friendship} from "../../api/friends/friendship";
 import {FriendApi} from "../../api/friends/friendApi";
 import {FriendStore} from "../../store/friends/friendStore";
+import {FriendRequest} from "../../api/friends/friendRequest";
 
 // TODO: there's no token storage.
 const token = "";
@@ -21,5 +22,33 @@ export class FriendService {
         const friends = await FriendApi.getAllFriends(token);
         FriendStore.setAllFriends(friends);
         return friends;
+    }
+
+    public static async getSentRequests(refresh: boolean = false): Promise<Array<FriendRequest>> {
+        if (!FriendStore.sentRequests || refresh) {
+            return FriendService.fetchSentRequests();
+        }
+
+        return FriendStore.sentRequests;
+    }
+
+    public static async fetchSentRequests(): Promise<Array<FriendRequest>> {
+        const sentRequests = await FriendApi.getSentFriendRequests(token);
+        FriendStore.setSentRequests(sentRequests);
+        return sentRequests;
+    }
+
+    public static async getReceivedRequests(refresh: boolean = false): Promise<Array<FriendRequest>> {
+        if (!FriendStore.receivedRequests || refresh) {
+            return FriendService.fetchReceivedRequests();
+        }
+
+        return FriendStore.receivedRequests;
+    }
+
+    public static async fetchReceivedRequests(): Promise<Array<FriendRequest>> {
+        const receivedRequests = await FriendApi.getReceivedFriendRequests(token);
+        FriendStore.setReceivedRequests(receivedRequests);
+        return receivedRequests;
     }
 }
--- src/store/friends/friendStore.ts
+++ src/store/friends/friendStore.ts
@@ -1,9 +1,22 @@
 import {Friendship} from "../../api/friends/friendship";
+import {FriendRequest} from "../../api/friends/friendRequest";
 
 export class FriendStore {
     public static allFriends: Array<Friendship> | undefined = undefined;
 
+    public static sentRequests: Array<FriendRequest> | undefined = undefined;
+
+    public static receivedRequests: Array<FriendRequest> | undefined = undefined;
+
     public static setAllFriends(friends: Array<Friendship> | undefined) {
         FriendStore.allFriends = friends;
+    }
+
+    public static setSentRequests(sentRequests: Array<FriendRequest> | undefined) {
+        FriendStore.sentRequests = sentRequests;
+    }
+
+    public static setReceivedRequests(receivedRequests: Array<FriendRequest> | undefined) {
+        FriendStore.receivedRequests = receivedRequests;
     }
 }
