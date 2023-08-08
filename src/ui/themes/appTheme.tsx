598524192d3ff7a0a4a53f3b6f1c96b1e42686d5
Kacper Faber <kacperf1234@gmail.com>
2023-08-08 12:38:25 +0200

    Responsive Web Design...
    1. index.html - added viewport.
    2. appTheme.tsx - MuiIcon font size is 2rem.

--- src/ui/themes/appTheme.tsx
+++ src/ui/themes/appTheme.tsx
@@ -1,9 +1,22 @@
 import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import React, {ReactNode} from "react";
 
-export const AppTheme = createTheme({palette: {mode: "dark"}});
+export const AppTheme = createTheme(
+    {
+        palette: {mode: "dark"},
+        components: {
+            MuiSvgIcon: {
+                styleOverrides: {
+                    root: {
+                        fontSize: '2rem'
+                    }
+                }
+            }
+        },
+    }
+);
 
-export const AppThemeProvider = ({children}: {children: ReactNode}) => {
+export const AppThemeProvider = ({children}: { children: ReactNode }) => {
     return (
         <ThemeProvider theme={AppTheme}>
             <>
