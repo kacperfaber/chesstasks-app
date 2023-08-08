392ebe6679dcfa20b0765a3fa3f22b6293b52cae
Kacper Faber <kacperf1234@gmail.com>
2023-08-08 11:08:45 +0200

    I believe I added MUI, and styled-components.

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
+        alias: {
+            '@mui/styled-engine': '@mui/styled-engine-sc'
+        }
     },
     module: {
         rules: [
