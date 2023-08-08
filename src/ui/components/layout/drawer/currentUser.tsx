ca7a6b346f0a97ae76a6ec8196e423abd97ca174
Kacper Faber <kacperf1234@gmail.com>
2023-08-08 13:46:18 +0200

    Created currentUser [AppDrawer_CurrentUser]
    Is lichess style header for the Drawer, shows h3 username or login button. [actually it's not implemented perfectly]

---
+++ src/ui/components/layout/drawer/currentUser.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import {Button} from "@mui/material";
+
+interface AppDrawer_CurrentUserArgs {
+    username: string | undefined,
+    logout: () => void;
+    login: () => void;
+}
+
+export const AppDrawer_CurrentUser = (args: AppDrawer_CurrentUserArgs) => {
+    const Block = styled.div`
+      width: 100%;
+      background-color: #454545;
+    `;
+
+    const InlineBlock = styled.div`
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    `;
+
+    // TODO: When AppDrawer_CurrentUser is showing login button, there's no padding.
+
+    // TODO: Add some kinda submenu, or drawer from down [ when user is logged in, we need to show to him some options like logout]
+
+    return (<Block>
+        <InlineBlock>
+            {
+                args.username ?
+                    <InlineBlock>
+                        <h3>{args.username}</h3>
+                    </InlineBlock>
+                    : <Button variant={'contained'}>Login In</Button>
+            }
+        </InlineBlock>
+    </Block>);
+}
