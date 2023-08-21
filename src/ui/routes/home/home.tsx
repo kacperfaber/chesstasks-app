4514a9fb8fcb0db1e0b28b472bfaf93d36b3356a
Kacper Faber <kacperf1234@gmail.com>
2023-08-21 16:03:48 +0200

    home.tsx - added AppLayout and information about that, the HOME is not implemented.

--- src/ui/routes/home/home.tsx
+++ src/ui/routes/home/home.tsx
@@ -1,4 +1,12 @@
+import {AppLayout} from "../../components/layout/appLayout";
+
 export const Home = () => {
     // TODO: Not implemented
-    return <h1>Home</h1>;
+    return (
+        <AppLayout title={'Test'}>
+
+           <h1>/ is not implemented yet.</h1>
+
+        </AppLayout>
+    )
 }
