e7bb0a89f56fedff66d92fb29320f03cd560183b
Kacper Faber <kacperf1234@gmail.com>
2023-08-25 10:43:31 +0200

    AppDrawer menu will only appear if user is logged in.

--- src/ui/components/layout/drawer/appDrawer.tsx
+++ src/ui/components/layout/drawer/appDrawer.tsx
@@ -8,21 +8,16 @@ import {Links} from "../../../../links";
 import {History, Home, LocalPizza, People, Person, Search, Style} from "@mui/icons-material";
 import {useTranslation} from "react-i18next";
 import {AuthenticationService} from "../../../../services/authentication/authenticationService";
+import {AppDrawer_List} from "./appDrawerList";
 
 interface AppDrawerArgs {
     isOpen: boolean;
     onClose: () => void;
 }
 
-const TransparentListSubheader = ({children}: {children: string}) => {
-    return <ListSubheader sx={{bgcolor: 'transparent', my: '10px'}}>{children}</ListSubheader>;
-}
-
 export const AppDrawer = (args: AppDrawerArgs) => {
     // TODO: Instead this use data from 'currentUserContext'
     const userContext = useContext(CurrentUserContext);
-
-    const {t} = useTranslation();
 
     const nav = useNavigate();
 
@@ -37,68 +32,8 @@ export const AppDrawer = (args: AppDrawerArgs) => {
     return (
         <Drawer anchor={'left'} open={args.isOpen} onClose={args.onClose}>
             <AppDrawer_CurrentUser login={ () => nav(Links.Login) } logout={ logout } username={userContext.value?.username}/>
-            <List>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Home/>
-                    </ListItemIcon>
-                    {t("drawer.home")}
-                </ListItemButton>
 
-                <TransparentListSubheader>
-                    {t("drawer.puzzle-title")}
-                </TransparentListSubheader>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Search/>
-                    </ListItemIcon>
-                    {t("drawer.random-puzzle")}
-                </ListItemButton>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Style/>
-                    </ListItemIcon>
-                    {t("drawer.puzzle-by-theme")}
-                </ListItemButton>
-
-                <TransparentListSubheader>
-                    {t("drawer.history-title")}
-                </TransparentListSubheader>
-
-                <ListItemButton onClick={() => nav(Links.PuzzleHistory)}>
-                    <ListItemIcon>
-                        <History/>
-                    </ListItemIcon>
-                    {t("drawer.history")}
-                </ListItemButton>
-
-                <TransparentListSubheader>
-                    {t("drawer.friends-title")}
-                </TransparentListSubheader>
-
-                <ListItemButton onClick={() => nav(Links.AllFriends)}>
-                    <ListItemIcon>
-                        <People/>
-                    </ListItemIcon>
-                    {t("drawer.friends")}
-                </ListItemButton>
-
-                <ListItemButton onClick={() => nav(Links.AllFriendRequests)}>
-                    <ListItemIcon>
-                        <People/>
-                    </ListItemIcon>
-                    {t("drawer.friend-requests")}
-                </ListItemButton>
-
-                <ListItemButton onClick={() => nav(Links.SearchUser)}>
-                    <ListItemIcon>
-                        <Search/>
-                    </ListItemIcon>
-                    {t("drawer.search-user")}
-                </ListItemButton>
-            </List>
+            {userContext.value ? <AppDrawer_List/> : null}
         </Drawer>
     )
 }
---
+++ src/ui/components/layout/drawer/appDrawerList.tsx
@@ -0,0 +1,75 @@
+import {List, ListItemButton, ListItemIcon} from "@mui/material";
+import {History, Home, People, Search, Style} from "@mui/icons-material";
+import {Links} from "../../../../links";
+import {TransparentListSubHeader} from "../../../base/list/transparentListSubHeader";
+import {useTranslation} from "react-i18next";
+import {useNavigate} from "react-router-dom";
+
+export const AppDrawer_List = () => {
+    const {t} = useTranslation();
+    const nav = useNavigate();
+    return (
+        <List>
+            <ListItemButton>
+                <ListItemIcon>
+                    <Home/>
+                </ListItemIcon>
+                {t("drawer.home")}
+            </ListItemButton>
+
+            <TransparentListSubHeader>
+                {t("drawer.puzzle-title")}
+            </TransparentListSubHeader>
+
+            <ListItemButton>
+                <ListItemIcon>
+                    <Search/>
+                </ListItemIcon>
+                {t("drawer.random-puzzle")}
+            </ListItemButton>
+
+            <ListItemButton>
+                <ListItemIcon>
+                    <Style/>
+                </ListItemIcon>
+                {t("drawer.puzzle-by-theme")}
+            </ListItemButton>
+
+            <TransparentListSubHeader>
+                {t("drawer.history-title")}
+            </TransparentListSubHeader>
+
+            <ListItemButton onClick={() => nav(Links.PuzzleHistory)}>
+                <ListItemIcon>
+                    <History/>
+                </ListItemIcon>
+                {t("drawer.history")}
+            </ListItemButton>
+
+            <TransparentListSubHeader>
+                {t("drawer.friends-title")}
+            </TransparentListSubHeader>
+
+            <ListItemButton onClick={() => nav(Links.AllFriends)}>
+                <ListItemIcon>
+                    <People/>
+                </ListItemIcon>
+                {t("drawer.friends")}
+            </ListItemButton>
+
+            <ListItemButton onClick={() => nav(Links.AllFriendRequests)}>
+                <ListItemIcon>
+                    <People/>
+                </ListItemIcon>
+                {t("drawer.friend-requests")}
+            </ListItemButton>
+
+            <ListItemButton onClick={() => nav(Links.SearchUser)}>
+                <ListItemIcon>
+                    <Search/>
+                </ListItemIcon>
+                {t("drawer.search-user")}
+            </ListItemButton>
+        </List>
+    )
+}
