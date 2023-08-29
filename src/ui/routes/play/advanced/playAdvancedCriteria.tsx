9596f9f059cfdd8a81f789731a8d0217eef8ac26
Kacper Faber <kacperf1234@gmail.com>
2023-08-29 16:41:51 +0200

    Implemented ResponsivePlayPuzzleContainer in PlayAdvancedCriteria route.

--- src/ui/routes/play/advanced/playAdvancedCriteria.tsx
+++ src/ui/routes/play/advanced/playAdvancedCriteria.tsx
@@ -3,10 +3,17 @@ import {Puzzle} from "../../../../api/puzzles/puzzle";
 import {useLoaderData} from "react-router-dom";
 import {AdvancedPlayCriteria} from "./advancedPlayCriteria";
 import {PlayPuzzleContainer} from "../../../components/puzzle/play/playPuzzleContainer";
+import {NextPuzzleType} from "../../../components/puzzle/play/playPuzzle";
+import {useMediaQuery, useTheme} from "@mui/material";
+import {MobilePlayPuzzle} from "../../../components/puzzle/play/mobile/mobilePlayPuzzle";
+import {DesktopPlayPuzzle} from "../../../components/puzzle/play/desktop/desktopPlayPuzzle";
 
 export const PlayAdvancedCriteria = () => {
     // Loader data
     const playCriteria = useLoaderData() as AdvancedPlayCriteria;
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
     const updateBuffer = (setBuffer: (arr: Array<Puzzle>) => void) => {
         PlayService.searchPuzzles(playCriteria.ranking, playCriteria.themeIds)
@@ -14,11 +21,18 @@ export const PlayAdvancedCriteria = () => {
             .catch(() => alert("cannot fetch using searchPuzzles"));
     }
 
+    const renderPuzzleComp = (puzzle: Puzzle, onNextPuzzleRequested: (type: NextPuzzleType) => void) => {
+        return isMobile
+            ? <MobilePlayPuzzle puzzle={puzzle} onNextPuzzleRequested={onNextPuzzleRequested}/>
+            : <DesktopPlayPuzzle puzzle={puzzle} onNextPuzzleRequested={onNextPuzzleRequested}/>
+    }
+
     return (
         <PlayPuzzleContainer
             onNextPuzzleRequested={() => {}}
             onGoodMove={() => {}}
             onBadMove={() => {}}
+            render={renderPuzzleComp}
             onBufferNeedsUpdate={updateBuffer}/>
     )
 }
