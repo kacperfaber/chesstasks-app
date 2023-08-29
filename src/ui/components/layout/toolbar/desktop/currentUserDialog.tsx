c7b5e8d7459a8c565ca529bfb87247987b1ab77a
Kacper Faber <kacperf1234@gmail.com>
2023-08-29 11:52:23 +0200

    Created DesktopAppToolbar
    Will be active only when screen size is x > MD [using MUI breakpoints].
    
    It has two icons on the right side, [or just login button when no user is logged in]. - for settings and current user.
    
    Current user will show a dialog, with current user data [and we can logout there]

---
+++ src/ui/components/layout/toolbar/desktop/currentUserDialog.tsx
@@ -0,0 +1,76 @@
+import {
+    Avatar,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText, Typography
+} from "@mui/material";
+import {useTranslation} from "react-i18next";
+import {useContext, useEffect} from "react";
+import {CurrentUserContext} from "../../../../contexts/authentication/currentUserContext";
+import {AuthenticationService} from "../../../../../services/authentication/authenticationService";
+import {Delete} from "@mui/icons-material";
+
+export const DesktopAppToolbar_CurrentUserDialog = ({open, onClose}: { open: boolean, onClose: () => void }) => {
+    const {t} = useTranslation();
+
+    const currentUserCtx = useContext(CurrentUserContext);
+
+    const logout = () => {
+        AuthenticationService.logout()
+            .then(() => currentUserCtx.setValue(undefined))
+            .catch(() => {}) // TODO
+    }
+
+    useEffect(() => {
+        AuthenticationService.getCurrentOrNull()
+            .then(currentUserCtx.setValue)
+            .catch(() => {
+            }) // TODO
+    }, []);
+
+    const closeClick = () => onClose();
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogContent>
+                <Typography variant={'h5'}>
+                    {t("layout.navbar.current-user.current-user")}
+                </Typography>
+
+                <List>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Avatar>
+                                {currentUserCtx.value?.username.charAt(0).toUpperCase()}
+                            </Avatar>
+                        </ListItemIcon>
+
+                        <ListItemText
+                            primary={currentUserCtx.value?.username}
+                            secondary={t("layout.navbar.current-user.this-user-logged-in")}/>
+                    </ListItemButton>
+
+                    <ListItemButton onClick={logout}>
+                        <ListItemIcon>
+                            <Delete/>
+                        </ListItemIcon>
+
+                        <ListItemText
+                            primary={t("all.logout")}/>
+                    </ListItemButton>
+                </List>
+            </DialogContent>
+
+            <DialogActions>
+                <Button variant={'text'} onClick={closeClick}>
+                    {t("all.close")}
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
---
+++ src/ui/components/layout/toolbar/desktop/desktopAppToolbar.tsx
@@ -0,0 +1,79 @@
+import {AppBar, Badge, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
+import {useTranslation} from "react-i18next";
+import {AccountCircle, Settings} from "@mui/icons-material";
+import {useNavigate} from "react-router-dom";
+import {DesktopAppToolbar_CurrentUserDialog} from "./currentUserDialog";
+import {useContext, useEffect, useState} from "react";
+import {Links} from "../../../../../links";
+import {CurrentUserContext} from "../../../../contexts/authentication/currentUserContext";
+import {AuthenticationService} from "../../../../../services/authentication/authenticationService";
+
+export const DesktopAppToolbar_UserActions = () => {
+    const nav = useNavigate();
+    const [currentUserDialog, setCurrentUserDialog] = useState(false);
+
+    const onAccountClicked = () => {
+        setCurrentUserDialog(true);
+    }
+
+    const settingsClick = () => {
+        nav(Links.Settings);
+    }
+
+    return (
+        <>
+            <IconButton onClick={settingsClick}>
+                <Settings/>
+            </IconButton>
+
+            <IconButton onClick={onAccountClicked}>
+                <Badge badgeContent={'4'} color={'primary'}>
+                    <AccountCircle/>
+                </Badge>
+            </IconButton>
+
+            <DesktopAppToolbar_CurrentUserDialog open={currentUserDialog}
+                                                 onClose={() => setCurrentUserDialog(false)}/>
+        </>
+    )
+}
+
+export const DesktopAppToolbar_AnonymousActions = () => {
+    const {t} = useTranslation();
+    const nav = useNavigate();
+
+    return (
+        <Button onClick={() => nav(Links.Login)} variant={'contained'}>{t("all.login-in")}</Button>
+    )
+}
+
+export const DesktopAppToolbar = () => {
+    const {t} = useTranslation();
+
+    const userCtx = useContext(CurrentUserContext);
+
+    useEffect(() => {
+        AuthenticationService.getCurrentOrNull()
+            .then(userCtx.setValue)
+            .catch(() => {
+            }) // TODO
+    }, []);
+
+    return (
+        <>
+            <AppBar position={'static'}>
+                <Toolbar>
+                    <Typography variant={'h5'}>
+                        {t("layout.navbar.title")}
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1}}></Box>
+
+                    {
+                        userCtx.value ? <DesktopAppToolbar_UserActions/> : <DesktopAppToolbar_AnonymousActions/>
+                    }
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
