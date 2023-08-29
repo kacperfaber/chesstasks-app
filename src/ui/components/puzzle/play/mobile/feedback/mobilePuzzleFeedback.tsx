bbf4684b4dc4bcae6fbed5913bd6e0c89592e662
Kacper Faber <kacperf1234@gmail.com>
2023-08-29 15:06:50 +0200

    MobilePlayPuzzle - Screen [not route] to play puzzle on mobile.
    Will be used in PlayPuzzleContainer [render method].
    
    it has just feedback, I want to add button to next [when it's solved], and bottom nav.

---
+++ src/ui/components/puzzle/play/mobile/feedback/mobilePuzzleFeedback.tsx
@@ -0,0 +1,105 @@
+import {PuzzleFeedbackValue} from "../../../feedback/puzzleFeedback";
+import {getColourToPlay} from "../../../../../../commons/puzzle/getColourToPlay";
+import {Puzzle} from "../../../../../../api/puzzles/puzzle";
+import {Typography} from "@mui/material";
+import {useTranslation} from "react-i18next";
+import {Check, Close} from "@mui/icons-material";
+import styled from "styled-components";
+
+export type MobilePuzzleFeedbackAttrs = {
+    puzzle: Puzzle,
+    feedback: PuzzleFeedbackValue,
+}
+
+const MobileFeedbackWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+`;
+
+export const MobilePuzzleFeedback_Start = ({puzzle, feedback}: MobilePuzzleFeedbackAttrs) => {
+    const {t} = useTranslation();
+
+    const col = getColourToPlay(puzzle.fen);
+
+    return (
+        <MobileFeedbackWrapper>
+            <Typography variant={'h5'}>
+                {t(`puzzle-feedback.find-best-move-for-${col}`)}
+            </Typography>
+        </MobileFeedbackWrapper>
+    )
+}
+
+export const MobilePuzzleFeedback_BadMove = () => {
+    const {t} = useTranslation();
+
+    return (
+        <MobileFeedbackWrapper>
+            <Close sx={{color: 'red'}}/>
+
+            <Typography variant={'h5'}>
+                {t(`puzzle-feedback.bad-move.title`)}
+            </Typography>
+
+            <Typography variant={'body2'} color={'text.secondary'}>
+                {t(`puzzle-feedback.bad-move.body`)}
+            </Typography>
+        </MobileFeedbackWrapper>
+    )
+}
+
+export const MobilePuzzleFeedback_GoodMove = () => {
+    const {t} = useTranslation();
+
+    return (
+        <MobileFeedbackWrapper>
+            <Typography variant={'h5'}>
+                {t(`puzzle-feedback.good-move.title`)}
+            </Typography>
+
+            <Typography variant={'body2'} color={'text.secondary'}>
+                {t(`puzzle-feedback.good-move.body`)}
+            </Typography>
+        </MobileFeedbackWrapper>
+    )
+}
+
+export const MobilePuzzleFeedback_Solved = () => {
+    const {t} = useTranslation();
+
+    return (
+        <MobileFeedbackWrapper>
+            <Check sx={{ color: 'green'}}/>
+
+            <Typography variant={'h5'}>
+                {t(`puzzle-feedback.solved.title`)}
+            </Typography>
+
+            <Typography variant={'body2'} color={'text.secondary'}>
+                {t(`puzzle-feedback.solved.body`)}
+            </Typography>
+        </MobileFeedbackWrapper>
+    )
+}
+
+export const MobilePuzzleFeedback = ({feedback, puzzle}: MobilePuzzleFeedbackAttrs) => {
+    if (feedback == "start") {
+        return <MobilePuzzleFeedback_Start feedback={feedback} puzzle={puzzle}/>
+    }
+
+    else if (feedback == "good_move") {
+        return <MobilePuzzleFeedback_GoodMove />
+    }
+
+    else if (feedback == "bad_move") {
+        return <MobilePuzzleFeedback_BadMove/>
+    }
+
+    else if (feedback == "solved") {
+        return <MobilePuzzleFeedback_Solved/>
+    }
+
+    // TODO: Never, unless feedback has only 'start', good_move, bad_move, solved
+    throw new Error("feedback is not supposed to have more than 'start', good_move, bad_move, solved")
+}
---
+++ src/ui/components/puzzle/play/mobile/mobilePlayPuzzle.tsx
@@ -0,0 +1,57 @@
+import {NextPuzzleType, PlayPuzzleAttrs} from "../playPuzzle";
+import {useState} from "react";
+import {PuzzleFeedbackValue} from "../../feedback/puzzleFeedback";
+import {SubmitResponse} from "../../../../../api/play/submitResponse";
+import {PlayService} from "../../../../../services/play/playService";
+import {PuzzleControllerResult} from "../../../../../services/puzzle/puzzleController";
+import {PuzzleBoard} from "../../../chess/board/puzzle/puzzleBoard";
+import {Grid, Typography} from "@mui/material";
+import {MobilePuzzleFeedback} from "./feedback/mobilePuzzleFeedback";
+
+export const MobilePlayPuzzle = (attrs: PlayPuzzleAttrs) => {
+    const [feedback, setFeedback] = useState<PuzzleFeedbackValue>("start")
+    const [nextPuzzleType, setNextPuzzleType] = useState<NextPuzzleType>("skip");
+
+    const [submitRes, setSubmitRes] = useState<SubmitResponse>();
+
+    const submit = () => {
+        if (submitRes) return;
+
+        PlayService.submitPuzzle(attrs.puzzle.id, feedback == "solved", ['e2e4']) // TODO.
+            .then(setSubmitRes)
+            .then(() => console.error("Submited bad moves"))
+            .catch(() => alert("Cannot submit"))
+    }
+
+    const onGoodMove = (r: PuzzleControllerResult) => {
+        attrs.onGoodMove?.(r);
+
+        if (r.finished) {
+            setFeedback("solved");
+            setNextPuzzleType("solved")
+            submit();
+            return;
+        }
+
+        setFeedback("good_move");
+    }
+
+    const onBadMove = (r: PuzzleControllerResult) => {
+        setFeedback("bad_move");
+        submit();
+        attrs.onBadMove?.(r);
+        setNextPuzzleType("skip");
+    }
+
+    return (
+        <Grid container style={{height: '100%'}}>
+            <Grid item xs={12}>
+                <PuzzleBoard puzzle={attrs.puzzle} onGoodMove={onGoodMove} onBadMove={onBadMove}/>
+            </Grid>
+
+            <Grid item xs={12} sx={{marginTop: '15px', height: '100%'}}>
+                <MobilePuzzleFeedback puzzle={attrs.puzzle} feedback={feedback}/>
+            </Grid>
+        </Grid>
+    );
+}
