6fa67a700c29bd3a1bd1eceb49c1c77c2855faf3
Kacper Faber <kacperf1234@gmail.com>
2023-09-02 15:20:13 +0200

    AdminManageUsers - make it under Grid.

--- src/ui/routes/admin/user/adminManageUsers.tsx
+++ src/ui/routes/admin/user/adminManageUsers.tsx
@@ -1,9 +1,37 @@
 import {AppLayout} from "../../../components/layout/appLayout";
+import {useState} from "react";
+import {Box, Grid, Tab, Tabs} from "@mui/material";
+import {useTranslation} from "react-i18next";
+import {AdminManageUsers_ListUserTab} from "./tabs/listUsers/listUserTab";
+
+export type AdminManageUsersTabs = "list_users";
 
 export const AdminManageUsers = () => {
+    const {t} = useTranslation();
+    const [tab, setTab] = useState<AdminManageUsersTabs>("list_users");
+
+    const onTabChanged = (event: React.SyntheticEvent, value: any) => {
+        const v = value as AdminManageUsersTabs;
+        setTab(v);
+    }
+
     return (
         <AppLayout title={''}>
-            <></>
+            <Grid container>
+                <Grid item xs={0} xl={1}></Grid>
+
+                <Grid item xs={12} xl={10}>
+                    <Box>
+                        <Tabs value={tab} onChange={onTabChanged}>
+                            <Tab label={t("_admin.manage-users.tabs.list-users")} value={"list_users"}></Tab>
+                        </Tabs>
+                    </Box>
+
+                    {
+                        tab == "list_users" ? <AdminManageUsers_ListUserTab/> : null
+                    }
+                </Grid>
+            </Grid>
         </AppLayout>
     )
 }
