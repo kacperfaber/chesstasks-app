86a8a5e68b85d2d77e0a7f13c2a1f5affb3727b1
Kacper Faber <kacperf1234@gmail.com>
2023-09-02 18:32:30 +0200

    searchPuzzleAdvanced.tsx rebuilt.
    It's using fab, and it looks much better.

--- src/ui/routes/puzzle/search/advanced/searchPuzzleAdvanced.tsx
+++ src/ui/routes/puzzle/search/advanced/searchPuzzleAdvanced.tsx
@@ -1,14 +1,21 @@
 import {AppLayout} from "../../../../components/layout/appLayout";
 import {useTranslation} from "react-i18next";
-import {Accordion, AccordionDetails, AccordionSummary, Button, Grid, Typography} from "@mui/material";
+import {Accordion, AccordionDetails, AccordionSummary, Avatar, Box, Fab, Grid, Typography} from "@mui/material";
 import {SearchPuzzleAdvanced_ThemeList} from "./themeList/themeList";
 import React, {useState} from "react";
 import {RankingRangeHolder, RankingSlider} from "./rankingSlider";
 import {useNavigate} from "react-router-dom";
 import {Links} from "../../../../../links";
-import {ExpandMore} from "@mui/icons-material";
+import {ExpandMore, PlayArrowSharp, SearchSharp} from "@mui/icons-material";
+import styled from "styled-components";
 
-// TODO: Make this component pretier.
+const SearchPuzzleAdvancedWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
 export const SearchPuzzleAdvanced = () => {
     const {t} = useTranslation();
@@ -33,31 +40,62 @@ export const SearchPuzzleAdvanced = () => {
 
     return (
         <>
-        <AppLayout title={t("")} mobile={{selectedNav: "search-advanced", showNav: true}}>
-            <Grid container>
-                <Grid item xs={12}>
-                    <RankingSlider holder={rankingHolder}/>
+            <AppLayout title={t("")} mobile={{selectedNav: "search-advanced", showNav: true}}>
+                <Grid container>
+                    <Grid item xs={12}>
+
+                    </Grid>
+
+                    <Grid item xs={12}>
+
+                    </Grid>
+
+                    <Grid item xs={12}>
+
+                    </Grid>
                 </Grid>
 
-                <Grid item xs={12}>
-                    <Button onClick={play} variant={'contained'}>{t("_all.play")}</Button>
-                </Grid>
 
-                <Grid item xs={12}>
-                    <Accordion>
-                        <AccordionSummary expandIcon={<ExpandMore/>}>
-                            {t("search-advanced.puzzle-themes")}
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <SearchPuzzleAdvanced_ThemeList
-                                selected={selectedThemes}
-                                select={selectTheme}
-                                unselect={unselectTheme}/>
-                        </AccordionDetails>
-                    </Accordion>
-                </Grid>
-            </Grid>
-        </AppLayout>
-    </>
+                <SearchPuzzleAdvancedWrapper style={{textAlign: 'center', padding: '15px'}}>
+                    <Avatar>
+                        <SearchSharp/>
+                    </Avatar>
+
+                    <Typography variant={'h5'}>{t("search-advanced.title")}</Typography>
+                    <Typography variant={'body2'} color={'text.secondary'}>{t("search-advanced.body")}</Typography>
+
+                    <Box sx={{width: '90%', marginLeft: 'auto', marginRight: 'auto', marginTop: '25px'}}>
+                        <RankingSlider holder={rankingHolder}/>
+                    </Box>
+
+                    <Box sx={{width: '90%', marginLeft: 'auto', marginRight: 'auto', marginTop: '25px'}}>
+                        <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} width={'100%'}>
+                            <Typography variant={'h5'}>{t("search-advanced.select-themes")}</Typography>
+                            <Typography variant={'body2'}
+                                        color={'text.secondary'}>{t("search-advanced.selected-themes", {all: selectedThemes.length})}</Typography>
+                        </Box>
+
+                        <Accordion>
+                            <AccordionSummary expandIcon={<ExpandMore/>}>
+                                {t("search-advanced.puzzle-themes")}
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <SearchPuzzleAdvanced_ThemeList
+                                    selected={selectedThemes}
+                                    select={selectTheme}
+                                    unselect={unselectTheme}/>
+                            </AccordionDetails>
+                        </Accordion>
+                    </Box>
+                </SearchPuzzleAdvancedWrapper>
+            </AppLayout>
+
+            <Fab onClick={play} sx={{position: 'absolute', right: 25, bottom: 75}} variant={'extended'} size={'medium'}
+                 color={'primary'}>
+                <PlayArrowSharp/>
+
+                {t("_all.play")}
+            </Fab>
+        </>
     )
 }
