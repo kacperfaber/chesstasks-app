9b9df84e224303374760c03fbb75e2fdc1f7c782
Kacper Faber <kacperf1234@gmail.com>
2023-09-02 13:28:55 +0200

    Fixed language for defaultDrawerList.
    Deleted _desktop and _mobile categories, items translations moved to _default section.

--- src/ui/components/layout/drawer/defaultDrawerList.tsx
+++ src/ui/components/layout/drawer/defaultDrawerList.tsx
@@ -9,92 +9,93 @@ import {PrivacyNav} from "../../../../commons/privacy/privacyNav";
 export function DefaultDrawerList() {
     const {t} = useTranslation();
     const nav = useNavigate();
+
     return (
         <List>
             <ListItemButton>
                 <ListItemIcon>
                     <Home/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.home")}
+                {t("_layout._default.drawer-list.home")}
             </ListItemButton>
 
             <TransparentListSubHeader>
-                {t("_layout._desktop.drawer-list.puzzle-title")}
+                {t("_layout._default.drawer-list.puzzle-title")}
             </TransparentListSubHeader>
 
             <ListItemButton onClick={() => nav(Links.playSimpleWithoutCriteria())}>
                 <ListItemIcon>
                     <Search/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.random-puzzle")}
+                {t("_layout._default.drawer-list.random-puzzle")}
             </ListItemButton>
 
             <ListItemButton onClick={() => nav(Links.AllThemes)}>
                 <ListItemIcon>
                     <Style/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.puzzle-by-theme")}
+                {t("_layout._default.drawer-list.puzzle-by-theme")}
             </ListItemButton>
 
             <ListItemButton onClick={() => nav(Links.SearchAdvanced)}>
                 <ListItemIcon>
                     <Style/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.puzzle-search-advanced")}
+                {t("_layout._default.drawer-list.puzzle-search-advanced")}
             </ListItemButton>
 
             <TransparentListSubHeader>
-                {t("_layout._desktop.drawer-list.history-title")}
+                {t("_layout._default.drawer-list.history-title")}
             </TransparentListSubHeader>
 
             <ListItemButton onClick={() => nav(Links.PuzzleHistory)}>
                 <ListItemIcon>
                     <History/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.history")}
+                {t("_layout._default.drawer-list.history")}
             </ListItemButton>
 
             <TransparentListSubHeader>
-                {t("_layout._desktop.drawer-list.friends-title")}
+                {t("_layout._default.drawer-list.friends-title")}
             </TransparentListSubHeader>
 
             <ListItemButton onClick={() => nav(Links.friends(null))}>
                 <ListItemIcon>
                     <People/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.friends")}
+                {t("_layout._default.drawer-list.friends")}
             </ListItemButton>
 
             <ListItemButton onClick={() => nav(Links.friends(null))}>
                 <ListItemIcon>
                     <People/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.friend-requests")}
+                {t("_layout._default.drawer-list.friend-requests")}
             </ListItemButton>
 
             <ListItemButton onClick={() => nav(Links.SearchUser)}>
                 <ListItemIcon>
                     <Search/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.search-user")}
+                {t("_layout._default.drawer-list.search-user")}
             </ListItemButton>
 
             <TransparentListSubHeader>
-                {t("_layout._desktop.drawer-list.privacy-title")}
+                {t("_layout._default.drawer-list.privacy-title")}
             </TransparentListSubHeader>
 
             <ListItemButton onClick={PrivacyNav.rodoStatement}>
                 <ListItemIcon>
                     <Search/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.rodo-statement")}
+                {t("_layout._default.drawer-list.rodo-statement")}
             </ListItemButton>
 
             <ListItemButton onClick={PrivacyNav.privacyStatement}>
                 <ListItemIcon>
                     <Search/>
                 </ListItemIcon>
-                {t("_layout._desktop.drawer-list.privacy-statement")}
+                {t("_layout._default.drawer-list.privacy-statement")}
             </ListItemButton>
         </List>
     )
