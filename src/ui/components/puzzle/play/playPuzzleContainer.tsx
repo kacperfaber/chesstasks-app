e607e8420c7c36ad6dc287b0aae18f0535772f11
Kacper Faber <kacperf1234@gmail.com>
2023-09-25 12:51:03 +0200

    Added CircualProgress to playPuzzleContainer.
    Backdrop with text and progress is active, when we're loading the buffer.

--- src/ui/components/puzzle/play/playPuzzleContainer.tsx
+++ src/ui/components/puzzle/play/playPuzzleContainer.tsx
@@ -3,6 +3,8 @@ import {Puzzle} from "../../../../api/puzzles/puzzle";
 import {useEffect, useState} from "react";
 import {NextPuzzleType} from "../feedback/puzzleFeedbackValue";
 import {PlayPuzzleContainer_SuchEmpty} from "./suchEmpty";
+import { Backdrop, CircularProgress, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export type PlayPuzzleContainerAttrs = {
     onGoodMove?: (p: Puzzle, r: PuzzleControllerResult) => void;
@@ -17,6 +19,7 @@ export const PlayPuzzleContainer = (attrs: PlayPuzzleContainerAttrs) => {
     const [currentPuzzle, setCurrentPuzzle] = useState<Puzzle>();
     const [index, setIndex] = useState(0);
     const [buffer, setBuffer] = useState<Array<Puzzle>>();
+    const {t} = useTranslation();
 
     const reloadBuffer = () => {
         attrs.onBufferNeedsUpdate?.(setBuffer);
@@ -53,6 +56,20 @@ export const PlayPuzzleContainer = (attrs: PlayPuzzleContainerAttrs) => {
 
     return (
         <>
+            {
+                <Backdrop open={(buffer?.length ?? 0) == 0} sx={{zIndex: 999999, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                    <Typography variant={'h5'}>
+                      {t("play-puzzle-container._comps.loading.title")}
+                    </Typography>
+
+                    <Typography variant={'body2'}>
+                      {t("play-puzzle-container._comps.loading.body")}
+                    </Typography>
+
+                    <CircularProgress color={"inherit"}/>
+                </Backdrop>
+            }
+
             {
                 buffer?.length == 0 ? <PlayPuzzleContainer_SuchEmpty/> : null
             }
